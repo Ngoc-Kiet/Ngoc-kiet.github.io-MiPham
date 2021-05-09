315fae0089f382a2820910c67eec077dc8e6d370
Ngoc-Kiet <76905291+Ngoc-Kiet@users.noreply.github.com>
2021-05-09 12:51:30 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-const config = require('./config');
 const mongoose = require('mongoose')
 
 mongoose.Promise = require('bluebird');
 mongoose.Promise = global.Promise;
 
-
+module.exports = {
+  app: {
+   port = normalizePort(process.env.PORT || '3000')
+  },
+  database: {
+    connection: "mongodb+srv://ngockiet:ngockiet123123@cluster0.yle50.mongodb.net/shopping?retryWrites=true&w=majority",
+    option: 	{
+      useNewUrlParser:true,
+      useUnifiedTopology: true
+    }
+  },
+  session: {
+    key: '27bda112-99dd-4496-8015-ea20d1034228'
+  }
+};
 const connection = mongoose.connect(config.database.connection, config.database.option)
 	.then(() => {
 		console.log("Connected to the database!");
